2a5ee1e6a4103e6d7fe3d7bd2003bc4e64b66a4c
Nikolay Burlov <kohgpat@gmail.com>
2018-04-19 03:11:01 +0800

    Fix formatting

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,97 +1,92 @@
-// const mocks = require("./mocks");
-const { getBooks, getAvailableBooks, getBookById, saveBook, updateBook } = require("./models/book");
+const {
+  getBooks,
+  getAvailableBooks,
+  getBookById,
+  saveBook,
+  updateBook,
+} = require("./models/book");
 const { getAvailableSouvenirs, getSouvenirById } = require("./models/souvenir");
-const { getOrders, getOrderById, saveOrder, updateOrder } = require("./models/order");
+const {
+  getOrders,
+  getOrderById,
+  saveOrder,
+  updateOrder,
+} = require("./models/order");
 
 const resolvers = {
   Query: {
     book(id) {
-      return getBookById(id)
-        .then(result => {
-          return result;
-        });
+      return getBookById(id).then(result => {
+        return result;
+      });
     },
     books() {
-      return getBooks()
-        .then(result => {
-          return result;
-        });
+      return getBooks().then(result => {
+        return result;
+      });
     },
     availableBooks() {
-      return getAvailableBooks()
-        .then(result => {
-          return result;
-        });
+      return getAvailableBooks().then(result => {
+        return result;
+      });
     },
     souvenir(id) {
-      return getSouvenirById(id)
-        .then(result => {
-          return result;
-        });
+      return getSouvenirById(id).then(result => {
+        return result;
+      });
     },
     souvenirs() {
-      return getAvailableSouvenirs()
-        .then(result => {
-          return result;
-        });
+      return getAvailableSouvenirs().then(result => {
+        return result;
+      });
     },
     order(id) {
-      return getOrderById(id)
-        .then(result => {
-          return result;
-        });
+      return getOrderById(id).then(result => {
+        return result;
+      });
     },
     orders() {
-      return getOrders()
-        .then(result => {
-          return result;
-        });
-    }
+      return getOrders().then(result => {
+        return result;
+      });
+    },
   },
   Mutation: {
     addBook(_, { book }) {
-      return saveBook(book)
-        .then(result => {
-          // Get generated key from rethinkdb
-          const id = result.generated_keys[0];
+      return saveBook(book).then(result => {
+        // Get generated key from rethinkdb
+        const id = result.generated_keys[0];
 
-          return getBookById(id)
-            .then(result => {
-              return result;
-            });
+        return getBookById(id).then(result => {
+          return result;
         });
+      });
     },
     updateBook(_, { id, book }) {
-      return updateBook(id, book)
-        .then(result => {
-          return getBookById(id)
-            .then(result => {
-              return result;
-            });
+      return updateBook(id, book).then(result => {
+        return getBookById(id).then(result => {
+          return result;
         });
+      });
     },
     addOrder(_, { order }) {
-      return saveOrder(order)
-        .then(result => {
-          // Get generated key from rethinkdb
-          const id = result.generated_keys[0];
+      return saveOrder(order).then(result => {
+        // Get generated key from rethinkdb
+        const id = result.generated_keys[0];
 
-          return getOrderById(id)
-            .then(result => {
-              return result;
-            });
+        return getOrderById(id).then(result => {
+          return result;
         });
+      });
     },
     updateOrder(_, { id, order }) {
-      return updateOrder(id, order)
-        .then(result => {
-          return getOrderById(id)
-            .then(result => {
-              return result;
-            });
+      return updateOrder(id, order).then(result => {
+        return getOrderById(id).then(result => {
+          return result;
         });
+      });
     },
-  }
+  },
 };
 
 module.exports = resolvers;
--- server/schema.js
+++ server/schema.js
@@ -1,6 +1,4 @@
-const {
-  makeExecutableSchema
-} = require("graphql-tools");
+const { makeExecutableSchema } = require("graphql-tools");
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
