cd836d05513f988e0583f172209e896d9eb8342b
Nikolay Burlov <kohgpat@gmail.com>
2018-04-19 15:19:13 +0800

    Add fake kassa for fake payment processing

---
+++ server/kassa/index.js
@@ -0,0 +1,40 @@
+const fetch = require("node-fetch");
+const nanoid = require("nanoid");
+
+const delay = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+class Kassa {
+  constructor(token) {
+    this.token = token;
+  }
+
+  async process(order, params = { shouldFail: false }) {
+    const processing = (details) => {
+      const request_id = nanoid();
+
+      if (params.shouldFail) {
+        return {
+          status: "failure",
+          request_id,
+          details
+        };
+      }
+
+      return {
+        status: "waiting",
+        request_id,
+        details
+      };
+    };
+
+    const response = await delay(200).then(() => processing(order));
+
+    console.log("response: ", response);
+
+    return response;
+  };
+}
+
+module.exports = Kassa;
