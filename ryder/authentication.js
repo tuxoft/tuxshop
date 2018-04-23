78e694865e33f997069a430b10ed846440bc17f6
Nikolay Burlov <kohgpat@gmail.com>
2018-04-23 18:56:04 +0800

    Add authentication using passport to api server

---
+++ ryder/authentication.js
@@ -0,0 +1,49 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+
+const init = () => {
+  // Configure the local strategy for use by Passport.
+  //
+  // The local strategy require a `verify` function which receives the credentials
+  // (`email` and `password`) submitted by the user.  The function must verify
+  // that the password is correct and then invoke `done` callback with a user object, which
+  // will be set at `req.user` in route handlers after authentication.
+  passport.use(new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+    const user = await User.getUser({ email });
+
+    if (!user) return done(null, false);
+    if (!User.authenticate(user, password)) return done(null, false);
+
+    console.log("User found: ", user);
+
+    return done(null, user);
+  }));
+
+  // Configure Passport authenticated session persistence.
+  //
+  // In order to restore authentication state across HTTP requests, Passport needs
+  // to serialize users into and deserialize users out of the session.  The
+  // typical implementation of this is as simple as supplying the user ID when
+  // serializing, and querying the user record by ID from the database when
+  // deserializing.
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    const user = await User.getUser({ id });
+
+    if (!user) return done("User not found");
+
+    done(null, user);
+
+    return null;
+  });
+
+  return passport;
+};
+
+module.exports = {
+  initPassport: init
+};
--- ryder/server.js
+++ ryder/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
+const session = require("express-session");
 const helmet = require("helmet");
+
+// Initialize authentication
+const passport = require("./authentication").initPassport();
 
 const schema = require("./schema");
 
@@ -14,11 +18,33 @@ const app = express();
 app.use(compression());
 app.use(helmet());
 
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "tuxshop secret asadsd",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Authentication
+app.post("/login", passport.authenticate("local"), (req, res) => {
+  console.log("login success");
+
+  res.redirect("http://localhost:3000");
+});
+
+// GraphiQL API
 app.use("/graphql", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
   );
 
   if (req.method === "OPTIONS") {
@@ -33,14 +59,14 @@ app.use(
   bodyParser.json(),
   graphqlExpress({
     schema,
-    tracing: true,
-  }),
+    tracing: true
+  })
 );
 
 app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 app.listen(GRAPHQL_PORT, () =>
   console.log(
-    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`,
-  ),
+    `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
+  )
 );
