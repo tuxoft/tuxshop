4b697fa3af18b0bf2d376919f7efd5a86e3fbbff
Nikolay Burlov <kohgpat@gmail.com>
2018-04-24 10:15:38 +0800

    Fix formatting in Cart

--- src/lib/Cart.js
+++ src/lib/Cart.js
@@ -1,82 +1,88 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import * as storage from "./localStorage";
 
 export const CartContext = React.createContext("cart");
 
 export class CartProvider extends Component {
-  addToCart = (product) => {
-    this.setState({
-      cart: {
-        ...this.state.cart,
-        products: [
-          ...this.state.cart.products,
-          product
-        ],
-        total: this.calculateTotal([
-          ...this.state.cart.products,
-          product
-        ])
-      }
-    }, () => {
-      storage.store("cart", this.state.cart);
-    });
+  addToCart = product => {
+    this.setState(
+      {
+        cart: {
+          ...this.state.cart,
+          products: [...this.state.cart.products, product],
+          total: this.calculateTotal([...this.state.cart.products, product]),
+        },
+      },
+      () => {
+        storage.store("cart", this.state.cart);
+      },
+    );
   };
 
-  removeFromCart = (product) => {
-    const productIdx = this.state.cart.products.findIndex(p => p.id === product.id);
+  removeFromCart = product => {
+    const productIdx = this.state.cart.products.findIndex(
+      p => p.id === product.id,
+    );
 
     if (productIdx < 0) {
       return false;
     }
 
-    this.setState({
-      cart: {
-        ...this.state.cart,
-        products: [
-          ...this.state.cart.products.slice(0, productIdx),
-          ...this.state.cart.products.slice(productIdx + 1)
-        ],
-        total: this.calculateTotal([
-          ...this.state.cart.products.slice(0, productIdx),
-          ...this.state.cart.products.slice(productIdx + 1)
-        ])
-      }
-    }, () => {
-      storage.store("cart", this.state.cart);
-    });
+    this.setState(
+      {
+        cart: {
+          ...this.state.cart,
+          products: [
+            ...this.state.cart.products.slice(0, productIdx),
+            ...this.state.cart.products.slice(productIdx + 1),
+          ],
+          total: this.calculateTotal([
+            ...this.state.cart.products.slice(0, productIdx),
+            ...this.state.cart.products.slice(productIdx + 1),
+          ]),
+        },
+      },
+      () => {
+        storage.store("cart", this.state.cart);
+      },
+    );
   };
 
-  inCart = (product) => {
-    const foundProduct = this.state.cart.products.find(p => p.id === product.id);
+  inCart = product => {
+    const foundProduct = this.state.cart.products.find(
+      p => p.id === product.id,
+    );
 
     return foundProduct ? true : false;
   };
 
-  calculateTotal = (products) => {
-    return products
-      .reduce((acc, current) => acc + current.price, 0);
+  calculateTotal = products => {
+    return products.reduce((acc, current) => acc + current.price, 0);
   };
 
   clearCart = () => {
-    this.setState({
-      cart: {
-        products: [],
-        total: 0
-      }
-    }, () => {
-      storage.store("cart", this.state.cart);
-    });
+    this.setState(
+      {
+        cart: {
+          products: [],
+          total: 0,
+        },
+      },
+      () => {
+        storage.store("cart", this.state.cart);
+      },
+    );
   };
 
   state = {
     cart: storage.load("cart") || {
       products: [],
-      total: 0
+      total: 0,
     },
     addToCart: this.addToCart,
     removeFromCart: this.removeFromCart,
     inCart: this.inCart,
-    clearCart: this.clearCart
+    clearCart: this.clearCart,
   };
 
   render() {
