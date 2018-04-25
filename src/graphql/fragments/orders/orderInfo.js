3ddba168c8e7d0cf3ced88ee5a084c8fcc1e0cc6
Nikolay Burlov <kohgpat@gmail.com>
2018-04-25 22:34:58 +0800

    Add orderInfo fragment

---
+++ src/graphql/fragments/orders/orderInfo.js
@@ -0,0 +1,12 @@
+import gql from "graphql-tag";
+
+export const orderInfo = gql`
+  fragment orderInfo on Order {
+    id
+    status
+    amount
+    email
+    paymentId
+    idempotenceKey
+  }
+`;
