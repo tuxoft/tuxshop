d71e70f2abc9a6b91b0a8d4b30adfd6e0b63c859
Nikolay Burlov <kohgpat@gmail.com>
2018-04-26 17:41:05 +0800

    Add Utils module

---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+import dayjs from "dayjs";
+
+const Utils = {
+  formatDate: (date, format = "DD.MM.YYYY") => {
+    if (!date) {
+      return null;
+    }
+
+    return dayjs(date).format(format);
+  }
+};
+
+export default Utils;
